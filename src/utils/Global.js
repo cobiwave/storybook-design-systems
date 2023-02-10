685f85b9695bc6f2c199e3d44a0ec280c5406899
Cobi <jacobojavier98@gmail.com>
2023-02-10 12:12:19 -0300

    globalStyle identation

--- src/utils/Global.js
+++ src/utils/Global.js
@@ -3,24 +3,27 @@ import { primaryFont } from "./typography";
 import { normalize } from "polished";
 
 export const GlobalStyle = createGlobalStyle`
-${normalize()}
-html {
-  box-sizing: border-box;
-  font-size: 16px;
-}
-*, *:before, *:after {
-  box-sizing: inherit;
-}
-body {
-  margin: 0;
-  padding: 65px 0 0;
-  font-family: ${primaryFont};
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
+  ${normalize()}
 
-main {
-  width: 90%;
-  margin: 0 auto;
-}
+  html {
+    box-sizing: border-box;
+    font-size: 16px;
+  }
+
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+  
+  body {
+    margin: 0;
+    padding: 65px 0 0;
+    font-family: ${primaryFont};
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  main {
+    width: 90%;
+    margin: 0 auto;
+  }
 `;
